36eed58dc683b964d5cdabd4ccc312cc7746571e
Mihail <mihailmelinte74@gmail.com>
2024-07-14 20:10:37 +0300

    Rework code

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 let container = document.querySelector(".container");
 let btn = document.querySelector(".sizeBtn");
+let gridDimension = 16;
 
-for(let i = 0; i < 256; i++) {
-    const color = randomRGB();
 
+function createGrid(gridDimension) {
+    container.textContent = "";
+    
+    for(let i = 0; i < gridDimension; i++) {
+        for(let j = 0; j < gridDimension; j++) {
+            createCells(gridDimension);
+        }
+    }
+}
+function createCells(gridDimension) {
+    const color = randomRGB();       
     let div = document.createElement("div");
+
     div.classList.add("change");
-    div.style.width = `${600/16}px`;
+    div.style.flex = `1 0 ${100 / gridDimension}%`;
     div.style.aspectRatio = "1";
     container.appendChild(div);
 
     div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = color;
+       div.style.backgroundColor = color;
     });
 }
 
+
+createGrid(gridDimension);
+
 btn.addEventListener("click", () => {
     let gridDimension = getValidNumber();
-    container.textContent = "";
-
-    for(let i = 0; i < gridDimension * gridDimension; i++) {
-        const color = randomRGB();
-        
-        let div = document.createElement("div");
-        div.classList.add("change");
-        div.style.width = `${600/gridDimension}px`;
-        div.style.aspectRatio = "1";
-        container.appendChild(div);
-
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = color;
-        });
-    }
+    
+    createGrid(gridDimension);
 });
 
 function randomColorCode() {
@@ -49,3 +50,7 @@ function getValidNumber() {
 
     return number;
 }
+
+
+
+
