3f7a245d7ad325bc133c27476af4051a5254bed1
Mihail <mihailmelinte74@gmail.com>
2024-07-14 13:34:59 +0300

    Add more functionality #2

--- script.js
+++ script.js
@@ -29,16 +29,3 @@ btn.addEventListener("click", () => {
         });
     }
 });
-
-
-
-
-/* for(let i = 0; i < gridDimension * gridDimension; i++) {
-    let div = document.createElement("div");
-    div.classList.add("change");
-    container.appendChild(div);
-
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "blue";
-    });
-} */
